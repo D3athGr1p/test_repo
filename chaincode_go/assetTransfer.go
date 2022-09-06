62e429a74aeea81aef9a740fdd49404107a579e4
D3athGr1p <xqwertyuiolkjhgfd@kdmail.com>
2022-09-06 14:13:20 +0530

    chaincode

---
+++ chaincode_go/assetTransfer.go
@@ -0,0 +1,23 @@
+/*
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package main
+
+import (
+	"log"
+
+	"github.com/hyperledger/fabric-contract-api-go/contractapi"
+	"github.com/D3athGr1p/test_repo/chaincode_go/chaincode"
+)
+
+func main() {
+	assetChaincode, err := contractapi.NewChaincode(&chaincode.Food_supplychain{})
+	if err != nil {
+		log.Panicf("Error creating asset-transfer-basic chaincode: %v", err)
+	}
+
+	if err := assetChaincode.Start(); err != nil {
+		log.Panicf("Error starting asset-transfer-basic chaincode: %v", err)
+	}
+}
